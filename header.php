1fa78f7a4f4650b4c9e92afb4c1ab869502bd2b6
Leshic <4tharraofdagon@gmail.com>
2017-12-11 20:02:37 +0200

    changes header image, logo, branding and description styles

--- header.php
+++ header.php
@@ -23,11 +23,17 @@
 <body <?php body_class(); ?>>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'humescores' ); ?></a>
+	<?php if ( get_header_image() && is_front_page() ) : ?>
+		<figure class="header-image">
+			<?php the_header_image_tag(); ?>
+		</figure>
+	<?php endif; ?>
 
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
+			<?php the_custom_logo(); ?>
+			<div class="site-branding__text">
 			<?php
-			the_custom_logo();
 			if ( is_front_page() && is_home() ) : ?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<?php else : ?>
@@ -40,6 +46,7 @@
 				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
 			<?php
 			endif; ?>
+			</div> <!--end .site-bronding-text -->
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
