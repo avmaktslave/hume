3218009b807ff5b2330a01539a10d849939ff949
Leshic <4tharraofdagon@gmail.com>
2017-12-12 21:59:21 +0200

    add some styles for images

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -122,3 +122,13 @@ function humescores_the_category_list() {
 		printf( '<span class="cat-links">' . esc_html__( '%1$s', 'humescores' ) . '</span>', $categories_list ); // WPCS: XSS OK.
 	}
 }
+
+/**
+ * Post navigation (previos / next post) for single posts
+ */
+function humescores_post_navigation() {
+	the_post_navigation( array(
+		'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'humescores' ) . '</span> ' . '<span class="screen-reader-text">' . __( 'Next post:', 'humescores' ) . '</span> ' . '<span class="post-title">%title</span>',
+		'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previos', 'humescores' ) . '</span> ' . '<span class="screen-reader-text">' . __( 'Previos post:', 'humescores' ) . '</span> ' . '<span class="post-title">%title</span>',
+		) );
+}
